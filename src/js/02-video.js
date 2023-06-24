cc4f30ee8f7ab2efd39a051f1a143fc9a164ed6f
Ro444elle <121978303+Ro444elle@users.noreply.github.com>
2023-06-24 16:58:03 +0300

    Update video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,29 @@
-import Vimeo from "@vimeo/player";
-import throttle from "lodash.throttle";
-import Player from "@vimeo/player/dist/player.js";
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player/dist/player.js';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 // const player = new Vimeo.Player(iframe);
 const player = new Player(iframe);
 
-player.on("play", function () {
-  console.log("played the video!");
+player.on('play', function () {
+  console.log('played the video!');
 });
 
 player.getVideoTitle().then(function (title) {
-  console.log("title:", title);
+  console.log('title:', title);
 });
 
 player.on(
-  "timeupdate",
+  'timeupdate',
   throttle(function (data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
   }, 1000)
 );
 
-const savedTime = parseFloat(localStorage.getItem("videoplayer-current-time"));
+const savedTime = parseFloat(localStorage.getItem('videoplayer-current-time'));
 if (!isNaN(savedTime)) {
   player.setCurrentTime(savedTime);
 }
+
+// *On play
